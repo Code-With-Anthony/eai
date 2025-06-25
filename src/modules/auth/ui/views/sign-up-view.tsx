a26bac1a8732a7d031b795f6fe0d70e9b09d4979
Anthony Dourado <anthony.dourado@thegatewaycorp.co.in>
2025-06-25 15:08:43 +0530

    05-authentication-socials

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,17 +1,18 @@
 "use client";
-import { Card, CardContent } from "@/components/ui/card";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Alert, AlertTitle } from "@/components/ui/alert";
-import { OctagonAlertIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { OctagonAlertIcon } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { z } from "zod";
 
 const formSchema = z.object({
     name: z.string().min(1, { message: "Name is required" }),
@@ -24,7 +25,6 @@ const formSchema = z.object({
 });
 
 export const SignUpView = () => {
-
     const router = useRouter();
     const [error, setError] = useState<string | null>(null);
     const [pending, setPending] = useState(false);
@@ -47,11 +47,12 @@ export const SignUpView = () => {
                 name: data.name,
                 email: data.email,
                 password: data.password,
+                callbackURL: "/",
             },
             {
                 onSuccess: () => {
                     setPending(false);
-                    router.push("/");
+                    router.push('/');
                 },
                 onError: ({ error }) => {
                     setPending(false);
@@ -60,7 +61,26 @@ export const SignUpView = () => {
             }
         );
     }
-    console.log("pending: ", pending);
+
+    const onSocial = (provider: "github" | "google") => {
+        setError(null);
+        setPending(true);
+        authClient.signIn.social(
+            {
+                provider: provider,
+                callbackURL: "/",
+            },
+            {
+                onSuccess: () => {
+                    setPending(false);
+                },
+                onError: ({ error }) => {
+                    setPending(false);
+                    setError(error.message);
+                }
+            }
+        );
+    }
 
     return (
         <div className="flex flex-col gap-6">
@@ -146,7 +166,7 @@ export const SignUpView = () => {
                                     type="submit"
                                     disabled={pending}
                                 >
-                                    Sign in
+                                    Sign up
                                 </Button>
                                 <div
                                     className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -158,8 +178,9 @@ export const SignUpView = () => {
                                         className="w-full"
                                         type="button"
                                         disabled={pending}
+                                        onClick={() => onSocial('google')}
                                     >
-                                        {/* <img src="/google.svg" alt="Google" className="h-4 w-4 mr-2" /> */}
+                                        <FaGoogle className="h-4 w-4 mr-2" />
                                         Google
                                     </Button>
                                     <Button
@@ -167,8 +188,9 @@ export const SignUpView = () => {
                                         className="w-full"
                                         type="button"
                                         disabled={pending}
+                                        onClick={() => onSocial('github')}
                                     >
-                                        {/* <img src="/github.svg" alt="GitHub" className="h-4 w-4 mr-2" /> */}
+                                        <FaGithub className="h-4 w-4 mr-2" />
                                         GitHub
                                     </Button>
                                 </div>
@@ -187,7 +209,7 @@ export const SignUpView = () => {
                     <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
                         <p className="text-2xl font-semibold text-white">
-                            EAi
+                            E.AI
                         </p>
                     </div>
                 </CardContent>
